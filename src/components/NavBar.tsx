e8e73a15c4ed1eb04221876df9c4867ac7b4619d
Sabalan Alaeiyeh <s.alaeiyeh@students.uu.nl>
2024-10-01 16:40:08 +0200

    `ben

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 
 function NavBar() {
-  <div className="navBar">
-    <p>
-      LABASAD
-      Barcelona School
-      of Arts & Design
-    </p>
-  </div>;
+  return (
+    <div className="container">
+        <div className="column">
+            <p>Column 1, Row 1</p>
+            <p>Column 1, Row 2</p>
+            <p>Column 1, Row 3</p>
+            <p>Column 1, Row 4</p>
+        </div>
+        <div className="column">
+            <p>Column 2, Row 1</p>
+            <p>Column 2, Row 2</p>
+            <p>Column 2, Row 3</p>
+            <p>Column 2, Row 4</p>
+        </div>
+    </div>
+  );
 }
 
 export default NavBar;
