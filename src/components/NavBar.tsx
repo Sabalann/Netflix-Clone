e0adae8ad2e77aae82b3fa9f1b362d17f5892d2a
Sabalan Alaeiyeh <s.alaeiyeh@students.uu.nl>
2024-10-01 16:38:02 +0200

    intial

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function NavBar() {
+  <div className="navBar">
+    <p>
+      LABASAD
+      Barcelona School
+      of Arts & Design
+    </p>
+  </div>;
+}
+
+export default NavBar;
