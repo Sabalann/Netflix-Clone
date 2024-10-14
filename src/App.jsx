134762b5dc3fcd879f3a0c55e103eee11f1bff53
Sabalan Alaeiyeh <s.alaeiyeh@students.uu.nl>
2024-10-14 20:34:10 +0200

    Added a Featured Film component, needs further styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import DarkModeButton from './components/DarkModeButton';
 import './App.css'; // Import a CSS file for styling
 import NavBar from './components/NavBar';
 import MovieList from './components/MovieList';
+import FeaturedFilm from './components/FeaturedFilm';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <NavBar></NavBar>
       </div>
       <div className="hero dark-mode">
+        <FeaturedFilm></FeaturedFilm>
         <MovieList title={"Your Favorites"}></MovieList>
         <MovieList title={"Popular Now"}></MovieList>
         <MovieList title={"Top Rated"}></MovieList>
---
+++ src/components/FeaturedFilm.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function FeaturedFilm() {
+  return (
+    <div className="FeaturedFilm">
+      <iframe
+        src="https://www.youtube.com/embed/TqfaCAA1UeE"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        title="Featured Film"></iframe>
+    </div>
+  );
+}
+
+export default FeaturedFilm;
